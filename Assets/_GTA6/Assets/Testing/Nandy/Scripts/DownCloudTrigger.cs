f75b0e3fee0543ab54fa753de226ee7b493741f9
NandyArnold <143703535+NandyArnold@users.noreply.github.com>
2025-07-18 16:50:05 +0200

    Some Camera Details

--- Assets/_GTA6/Assets/Testing/Nandy/Scripts/DownCloudTrigger.cs
+++ Assets/_GTA6/Assets/Testing/Nandy/Scripts/DownCloudTrigger.cs
@@ -1,12 +1,21 @@
+using Unity.Cinemachine;
 using UnityEngine;
+using System.Collections;
 
 public class DownCloudTrigger : MonoBehaviour
 {
     [Header("Geyser Launch Settings")]
     public float downwardForce = 12f;
-  
-    public string playerTag = "Player";
+    public float slowMotionDuration = 0.2f; // Duration of slow motion effect
+    public float slowMotionScale = 0.3f; // Scale of time during slow motion
 
+    public string playerTag = "Player";
+    private CinemachineImpulseSource impulseSource;
+
+    private void Awake()
+    {
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
@@ -16,9 +25,29 @@ public class DownCloudTrigger : MonoBehaviour
             {
                 Vector3 launchDirection = Vector3.down * downwardForce;
                 rb.AddForce(launchDirection, ForceMode.VelocityChange);
+                CinemachineImpulseSource impulse = GetComponent<CinemachineImpulseSource>();
+                if (impulse != null)
+                    impulse.GenerateImpulse();
+
+                // Slow time
+                StartCoroutine(ShortSlowMotion());//  Trigger camera shake
 
                 Debug.Log("Grandma boosted by geyser!");
             }
         }
+    }
+
+    private IEnumerator ShortSlowMotion()
+    {
+        float originalTimeScale = Time.timeScale;
+        float originalFixedDeltaTime = Time.fixedDeltaTime;
+
+        Time.timeScale = slowMotionScale; // Slow down to x%
+        Time.fixedDeltaTime = 0.02f * Time.timeScale; // Adjust physics timing
+
+        yield return new WaitForSecondsRealtime(slowMotionDuration); // How long the slow-mo lasts in real time
+
+        Time.timeScale = originalTimeScale;
+        Time.fixedDeltaTime = originalFixedDeltaTime;
     }
 }
